2c1b02e850001b4604b0986d2e2bc782525e29ee
bkcheung <b.cheung@hotmail.com>
2024-03-13 19:28:05 -0400

    Add array sorting and removing duplicates

--- index.js
+++ index.js
@@ -1,5 +1,5 @@
-let sortedArr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
-console.log(tree(sortedArr).root);
+let arr = [1, 7, 4, 23, 8, 9, 4, 3, 5, 7, 9, 67, 6345, 324];
+console.log(tree(arr).root);
 
 function node(value, left = null, right = null){
     return{
@@ -9,20 +9,28 @@ function node(value, left = null, right = null){
     }
 }
 
-function tree(array){
+function tree(arr){
+    if(Array.isArray(arr)){
+        arr = arr.sort((a,b)=>a-b); //sort
+        let prev = '';
+        for(let i=0; i<arr.length; i++){
+            if(arr[i]===prev){ arr.splice(i,1);} //remove duplicates
+            prev = arr[i];
+        }
+    }
     return{
-        root: buildTree(array, 0, array.length - 1),
+        root: buildTree(arr, 0, arr.length - 1),
     }
 }
 
-function buildTree(array, start, end){
+function buildTree(arr, start, end){
     if(start > end){
         return null;
     }
     let mid = Math.round((start+end)/2);
     let treeNode = node();
-    treeNode.value = array[mid];
-    treeNode.left = buildTree(array.slice(0,mid),start,mid-1);
-    treeNode.right = buildTree(array.slice(mid+1,end),mid+1, end);
+    treeNode.value = arr[mid];
+    treeNode.left = buildTree(arr.slice(0,mid),start,mid-1);
+    treeNode.right = buildTree(arr.slice(mid+1,end),mid+1, end);
     return treeNode;
 }
